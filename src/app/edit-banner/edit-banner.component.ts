d1503d6748d08c3e743855bfb46eada49f4bf909
Hoa Nguyen <kevinhoa95@gmail.com>
2018-05-28 08:49:04 +0700

    fix update banner

--- src/app/edit-banner/edit-banner.component.ts
+++ src/app/edit-banner/edit-banner.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BannerService } from "../banner.service";
 import { HttpClient } from '@angular/common/http';
-import { Route, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-banner',
@@ -12,7 +12,7 @@ export class EditBannerComponent implements OnInit {
   bannerDetail: any;
 
 
-  constructor(private bannerService: BannerService, private httpClient: HttpClient, private activatedRoute: ActivatedRoute) { }
+  constructor(private bannerService: BannerService, private httpClient: HttpClient, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     let bannerId = this.activatedRoute.snapshot.params["id"];
@@ -31,9 +31,12 @@ export class EditBannerComponent implements OnInit {
     console.log(banner);
     this.bannerService.updateBanner(banner).subscribe(
       data => {
+        alert("Cap nhat banner thanh cong...")
+        this.router.navigate(['banners']);
         console.log(data);
       },
       error => {
+        alert("cap nhat khong thanh cong")
         console.log(error);
       }
     )
